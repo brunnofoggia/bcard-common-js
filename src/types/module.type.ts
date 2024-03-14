b074be67076ebdeae3e2a523a159610a80f1329b
bruno foggia <brunnofoggia@gmail.com>
2024-03-14 16:30:19 -0300

    feat(m3): added legacy m3 to enum

--- src/types/module.type.ts
+++ src/types/module.type.ts
@@ -1,6 +1,7 @@
 export enum BCARD_MODULE {
     DOCS = 'docs',
     BCARD_M1 = 'bcard_m1',
+    M3_LEGACY = 'm3',
     BCARD_M3 = 'bcard_m3',
     BCARD_M9 = 'bcard_m9',
     BCARD_CCB = 'bcard_ccb',
